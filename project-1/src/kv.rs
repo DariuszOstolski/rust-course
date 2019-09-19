630ab9eedf3f80e025e0afc47461f73a2a731dec
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2019-09-19 21:35:33 +0200

    Rust format

--- project-1/src/kv.rs
+++ project-1/src/kv.rs
@@ -1,13 +1,9 @@
 pub trait KvStore {
-    fn set(&mut self, key: String, value: String) {
-
-    }
+    fn set(&mut self, key: String, value: String) {}
 
     fn get(&mut self, key: String) -> Option<String> {
         None
     }
 
-    fn remove(&mut self, key: String) {
-
-    }
+    fn remove(&mut self, key: String) {}
 }
